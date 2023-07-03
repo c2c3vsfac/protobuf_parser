750c354a9fffc406255619330fbab539c27bd6bd
c2c3vsfac <108676747+c2c3vsfac@users.noreply.github.com>
2023-07-03 23:46:55 +0800

    修了个bug，长字符串还是很慢

--- proto3_parser.cpp
+++ proto3_parser.cpp
@@ -3,6 +3,8 @@
 #include <json/json.h>
 #include <variant>
 #include <algorithm>
+#include <unordered_map>
+#include <any>
 #include "base64.h"
 #include "proto3_parser.h"
 
@@ -11,16 +13,18 @@ Json::Value read_json_packet(const std::string& json_name)
 	std::ifstream ifs(json_name);
 	Json::Value root;
 	ifs >> root;
+    ifs.close();
 	return root;
 }
 
 Json::Value all_serial = read_json_packet("packet_serialization.json");
 Json::Value ucn = read_json_packet("ucn_serialization.json");
 
-std::pair<uint64_t, uint8_t> varint(int now_location, const char* byte_str)
+
+std::pair<uint64_t, uint8_t> varint(int now_location, const std::string& byte_str)
 {
     uint8_t offset = 0;
-    uint64_t data = byte_str[now_location] & 0b1111111;
+    uint64_t data = byte_str[now_location] & 0b1111111; // 待优化，要是头铁真塞一个uint64的数据，那结果就错了
     while (true) {
         if (byte_str[now_location] >> 7) {
             offset += 1;
@@ -60,28 +64,20 @@ bool IsBigEndian()
 	return *ch == 0x12;
 }
 
+bool is_big_endian = IsBigEndian();
+
 template <typename T>
 T bytes_to_num_le(const std::string& byte_str, size_t offset)
 {
     T value;
     std::memcpy(&value, byte_str.data() + offset, sizeof(T));
-    if (IsBigEndian())
+    if (is_big_endian)
     {
         const size_t size = sizeof(T);
         char* p = reinterpret_cast<char*>(&value);
         std::reverse(p, p + size);
     }
     return value;
-}
-
-template <typename T>
-Json::Value convert_map_to_json_value(std::map<std::string, T> map){
-    Json::Value json_value;
-    for (const auto& pair : map) 
-    {
-        json_value[pair.first] = pair.second;
-    }
-    return json_value;
 }
 
 std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& byte_str, const std::string& packet_id, const int& flag, Json::Value encoding_rules, Json::Value prop_names){
@@ -104,14 +100,16 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
         }
         else {
             data_type = byte_str[i] & 0b111;
-            std::pair<uint64_t, uint8_t> varint_res = varint(i, byte_str.c_str());
+            //std::pair<uint64_t, uint8_t> varint_res = varint(i, byte_str);
+            std::pair<uint64_t, uint8_t> varint_res = varint(0, byte_str.substr(i, 10));
             data_id = std::to_string(varint_res.first >> 3);
             i += varint_res.second;
             i += 1;
         }
         if (encoding_rules.isMember(data_id)){
             if (data_type == 0){
-                std::pair<uint64_t, uint8_t> varint_res = varint(i, byte_str.c_str());
+                //std::pair<uint64_t, uint8_t> varint_res = varint(i, byte_str);
+                std::pair<uint64_t, uint8_t> varint_res = varint(0, byte_str.substr(i, 10));
                 uint64_t data = varint_res.first;
                 offset = varint_res.second;
                 if (encoding_rules[data_id].isArray()) {
@@ -134,9 +132,9 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
                     else {
                         decode_data[prop_names[data_id].asString()] = data;
                     }
-                    i += offset;
-                    i += 1;
                 }
+                i += offset;
+                i += 1;
             }
             else if (data_type == 1){
                 std::string encode_type = encoding_rules[data_id].asString();
@@ -146,7 +144,7 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
                 }
                 else if (encode_type == "sfixed64") {
                     uint64_t num = bytes_to_num_le<uint64_t>(byte_str, i);
-                    int64_t data = (num >> 1) ^ -static_cast<int64_t>(num & 1);
+                    int64_t data = (num >> 1) ^ - static_cast<int64_t>(num & 1);
                     decode_data[prop_name] = data;
                 }
                 else if (encode_type == "fixed64") {
@@ -162,7 +160,7 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
                 }
                 else if (encode_type == "sfixed32") {
                     uint32_t num = bytes_to_num_le<uint32_t>(byte_str, i);
-                    int32_t data = (num >> 1) ^ -static_cast<int32_t>(num & 1);
+                    int32_t data = (num >> 1) ^ - static_cast<int32_t>(num & 1);
                     decode_data[prop_name] = data;
                 }
                 else if (encode_type == "fixed32") {
@@ -171,7 +169,8 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
                 i += 4;
             }
             else if (data_type == 2){
-                std::pair<uint64_t, uint8_t> varint_res = varint(i, byte_str.c_str());
+                /*std::pair<uint64_t, uint8_t> varint_res = varint(i, byte_str);*/
+                std::pair<uint64_t, uint8_t> varint_res = varint(0, byte_str.substr(i, 10));
                 offset = varint_res.second;
                 uint64_t length = varint_res.first;
                 i += offset;
@@ -185,7 +184,8 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
                     }
                     else if (encode_type == "bytes") {
                         prop_name = prop_names[data_id].asString();
-                        decode_data[prop_name] = Base64::base64_encode(byte_str.substr(i, length).c_str(), byte_str.substr(i, length).size());
+                        std::string sub_str = byte_str.substr(i, length);
+                        decode_data[prop_name] = Base64::base64_encode(sub_str.c_str(), sub_str.size());
                     }
                     else if (encode_type.starts_with("repeated_")) {
                         std::string data_type = encode_type.substr(9); // repeated_
@@ -266,9 +266,11 @@ std::variant<Json::Value, std::pair<int, Json::Value>> parse(const std::string& 
             }
         }
     }
-    
     return decode_data;
 }
+
+//using DataValue = std::variant<int, double, std::string, std::vector<std::any>, std::unordered_map<std::string, std::any>>;
+//using DecodeData = std::unordered_map<std::string, DataValue>;
 
 bool initialize = false;
 
@@ -292,20 +294,32 @@ const char* parser(const char* byte_str, int length, const char* packet_id) {
     Json::Value json_result = std::get<Json::Value>(parse_result);
     std::string str_result = Json::FastWriter().write(json_result);
     auto reslen = str_result.length();
-    auto res = new char[reslen];
+    auto res = new char[reslen+1];
     // Use memcpy to handle embedded nulls
-    memcpy_s(res, reslen, str_result.data(), reslen);
+    memcpy_s(res, reslen+1, str_result.data(), reslen);
+    res[reslen] = '\0';
     return res;
 }
 
 //int main() {
-//    for (auto& key : ucn.getMemberNames()) {
-//        all_serial[key] = ucn[key];
-//    }
-//    std::string byte_str = {"", 235};
-//    std::variant parse_result = parse(byte_str, "2107", 0, {}, {});
-//    Json::Value result = std::get<Json::Value>(parse_result);
-//    std::cout << result << std::endl;
+//    //std::string byte_str = "";
+//    std::ifstream file("long_string.bin", std::ios::binary);
 //
+//    std::stringstream buffer;
+//    buffer << file.rdbuf();
+//
+//    std::string fileContent = buffer.str();
+//    auto reslen = fileContent.length();
+//    auto res = new char[reslen + 1];
+//    // Use memcpy to handle embedded nulls
+//    memcpy_s(res, reslen + 1, fileContent.data(), reslen);
+//    res[reslen] = '\0';
+//    const char* result = parser(res, reslen, "675");
+//    //parser2(res, reslen, "675");
+//    //std::cout << result << std::endl;
 //    return 0;
+//    
+//    //std::cout << byte_str.length() << std::endl; 
+//    //const char* result = parser(byte_str.c_str(), 114043, "675");
+//    
 //}
